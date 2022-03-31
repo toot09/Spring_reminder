59499cff2262517cccc9e7ea50a2610344a5e4a1
ujoneMAC@HOME <03toot28@gmail.com>
2022-04-01 00:46:31 +0900

    thymeleaf is done

--- src/main/java/hello/itemservice/domain/item/ItemType.java
+++ src/main/java/hello/itemservice/domain/item/ItemType.java
@@ -1,10 +1,14 @@
 package hello.itemservice.domain.item;
 
 public enum ItemType {
-    BOOK("도서"), FOOD("도서"), ETC("도서");
+    BOOK("도서"), FOOD("식품"), ETC("기타");
     private final String description;
 
     ItemType(String description) {
         this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
     }
 }
